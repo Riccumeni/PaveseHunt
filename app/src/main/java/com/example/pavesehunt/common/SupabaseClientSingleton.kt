1fee9dd81537c378a790d0e5150bf2e36412e9c4
Riccumeni <77925342+Riccumeni@users.noreply.github.com>
2023-12-13 16:47:41 +0100

    Update SupabaseClientSingleton.kt

--- app/src/main/java/com/example/pavesehunt/common/SupabaseClientSingleton.kt
+++ app/src/main/java/com/example/pavesehunt/common/SupabaseClientSingleton.kt
@@ -6,12 +6,12 @@ import io.github.jan.supabase.gotrue.GoTrue
 import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseClientSingleton {
-    private val API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBieGdhamJ5c2dyY2VxeXlpZm1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE0MjIyMjUsImV4cCI6MjAxNjk5ODIyNX0.nWsG7AzUNBEl6yZaIYdpO9NY2OT8ODYUY-aR9VOSB78"
+    private val API_KEY = "API_KEY"
     private var client: SupabaseClient? = null
 
     fun getClient(): SupabaseClient{
         if(client == null){
-            client = createSupabaseClient("https://pbxgajbysgrceqyyifms.supabase.co/", API_KEY){
+            client = createSupabaseClient("URL", API_KEY){
                 install(Postgrest)
                 install(GoTrue)
             }
