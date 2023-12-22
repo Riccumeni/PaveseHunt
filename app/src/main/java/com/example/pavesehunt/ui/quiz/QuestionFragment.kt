d8665b409d1282c29047944b4cea53234714fcb0
Riccumeni <77925342+Riccumeni@users.noreply.github.com>
2023-12-22 15:55:33 +0100

    Complete questionScreen

--- app/src/main/java/com/example/pavesehunt/ui/quiz/QuestionFragment.kt
+++ app/src/main/java/com/example/pavesehunt/ui/quiz/QuestionFragment.kt
@@ -1,5 +1,7 @@
 package com.example.pavesehunt.ui.quiz
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.os.Handler
@@ -7,12 +9,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.Button
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.activityViewModels
 import com.example.pavesehunt.R
 import com.example.pavesehunt.common.Questions
 import com.example.testapp.domain.viewmodels.QuizViewModel
+import com.google.android.material.progressindicator.LinearProgressIndicator
 
 class QuestionFragment : Fragment() {
 
@@ -49,8 +54,23 @@ class QuestionFragment : Fragment() {
         var position = shared.getInt("indexQuestion", 0)
 
         val questionText = view.findViewById<TextView>(R.id.questionText)
+        val counterIndicator = view.findViewById<LinearProgressIndicator>(R.id.counterIndicator)
+        val openPoemDialogCard = view.findViewById<CardView>(R.id.openPoemCard)
+        val tutorialCard = view.findViewById<CardView>(R.id.tutorialCard)
 
         questionText.text = Questions.questions[position].question
+
+        viewModel.counter.observe(viewLifecycleOwner){
+            counterIndicator.progress = it
+        }
+
+        openPoemDialogCard.setOnClickListener {
+            showDialog()
+        }
+
+        tutorialCard.setOnClickListener {
+            showTutorialDialog()
+        }
 
         val buttons: List<Button> = listOf(
             view.findViewById(R.id.firstAnswerButton),
@@ -95,7 +115,7 @@ class QuestionFragment : Fragment() {
 
                 Handler().postDelayed({
                     Questions.questions[position].answers.forEachIndexed{index, s ->
-                        buttons[index].setBackgroundColor(0xFFC4C4C4.toInt())
+                        buttons[index].setBackgroundColor(0xFFCBB18C.toInt())
                         buttons[index].text = s
                     }
 
@@ -106,6 +126,51 @@ class QuestionFragment : Fragment() {
             }
         }
 
+    }
+
+    fun showDialog(){
+        val dialog = Dialog(requireView().context)
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(R.layout.poem_dialog)
+
+        val poemTextView = dialog.findViewById<TextView>(R.id.poemTextView)
+        val exitButton = dialog.findViewById<Button>(R.id.okDialogButton)
+
+        poemTextView.text = "Nel mezzo del cammin di nostra vita\n" +
+                "mi ritrovai per una selva oscura,\n" +
+                "ché la diritta via era smarrita.\n" +
+                "\n" +
+                "Ahi quanto a dir qual era è cosa dura\n" +
+                "esta selva selvaggia e aspra e forte\n" +
+                "che nel pensier rinova la paura!\n" +
+                "\n" +
+                "Tant’è amara che poco è più morte;\n" +
+                "ma per trattar del ben ch’i’ vi trovai,\n" +
+                "dirò de l’altre cose ch’i’ v’ ho scorte."
+
+        exitButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    fun showTutorialDialog(){
+        val dialog = Dialog(requireView().context)
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(R.layout.tutorial_poem_dialog)
+
+        val exitButton = dialog.findViewById<Button>(R.id.okDialogButton)
+
+        exitButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
     companion object {
